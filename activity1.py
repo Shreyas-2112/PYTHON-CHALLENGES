c148d9ebfd7afe8dd4e9931a11051ad8ff7e79af
Shreyas-2112 <sompurshreyas@gmail.com>
2026-05-20 15:43:13 +0530

    Add files via upload

---
+++ activity1.py
@@ -0,0 +1,18 @@
+S = 7
+T = 9
+U = 2
+V = 5
+W = 4
+X = (V+U) * W / T;
+print("Value of (V+U) *W / T is ", X)
+
+
+
+
+name = "Ritvik"
+age = 100-50
+
+if name == "Ritvik" or name == "shreyas" and age >= 60 :
+    print("HELLO!! WELCOME.")
+else :
+    print("GOOD BYE!!")
